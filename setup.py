e4b7e5c734eb97089ab8852f52aea609f2f918e3
artovv <142149913+artovv@users.noreply.github.com>
2025-06-23 05:09:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name="model_finetuning",
     version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
     package_data={
         "model_finetuning": ["weights/*.pt"],
     },
-)
